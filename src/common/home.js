74aa411f950dff6bf92ee351978830d161ed4e43
Mario Ruano <mrf1989@hotmail.com>
2022-12-23 12:30:04 +0100

    Implement song detail view

--- src/common/home.js
+++ src/common/home.js
@@ -1,17 +1,27 @@
+import { useState } from "react";
+import SearchForm from "../songs/components/SearchForm";
+import SongsContainer from "../songs/components/SongsContainer";
+
 function Home() {
-    return (
-        <div className="row">
-            <div className="col-8 offset-2">
-                <div className="input-group mb-3">
-                    <input type="text" className="form-control border-purple" placeholder="Search song" aria-label="Songs name" aria-describedby="Songs name" />
-                    <button className="btn border-purple text-purple" type="button" id="button-addon2"><i className="bi bi-search btn-purple"></i></button>
-                </div>
-            </div>
-            <div className="col-4 offset-4 text-center">
-                <button type="button" className="btn border-purple text-purple bg-blue">Possibility</button>
-            </div>
-        </div>
-    );
+  const [songs, setSongs] = useState([]);
+  const [spotifySongs, setSpotifySongs] = useState(null);
+
+  function handleSpotifySearchClick(results) {
+    setSongs(results.songs);
+    setSpotifySongs(results.spotify);
+  }
+
+  return (
+    <div>
+      <SearchForm
+        handleSpotifySearchClick={handleSpotifySearchClick}
+      ></SearchForm>
+      <SongsContainer
+        songs={songs}
+        spotifySongs={spotifySongs}
+      ></SongsContainer>
+    </div>
+  );
 }
 
 export default Home;
