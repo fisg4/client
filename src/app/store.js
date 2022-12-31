b164c7f089f8ccc3a27b88326376c11a1ac2485f
Mario Ruano <mrf1989@hotmail.com>
2022-12-31 13:26:55 +0100

    changes the song state management using redux

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { songsReducer } from "../songs/slices/songsSlice";
+import songsReducer from "../songs/slices/songsSlice";
 
 export const store = configureStore({
   reducer: {
--- src/songs/slices/songsSlice.js
+++ src/songs/slices/songsSlice.js
@@ -9,7 +9,19 @@ const initialState = {
 export const songsSlice = createSlice({
   name: "songs",
   initialState,
-  reducers: {},
+  reducers: {
+    setSongs: (state, action) => {
+      state.songs = action.payload;
+    },
+    toggleSpotify: (state, action) => {
+      state.spotify = action.payload;
+    },
+    saveQuery: (state, action) => {
+      state.query = action.payload;
+    },
+  },
 });
+
+export const { setSongs, toggleSpotify, saveQuery } = songsSlice.actions;
 
 export default songsSlice.reducer;
