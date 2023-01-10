bfad3693ca1f4e1403a0941d730c3035cf213916
carnuare <carnuare@gmail.com>
2023-01-10 20:17:04 +0100

    Like button working fine

---
+++ src/users/LikedSongs.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react';
+import Song from '../songs/components/Song';
+
+function LikedSongs() {
+  const [songs, setSongs] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (!token) return;
+
+    fetch('/api/v1/users/likes', {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+      .then(res => res.json())
+      .then(data => {
+        const songs = data.map(item => item.song);
+        setSongs(songs);
+      })
+      .catch(err => setError(err));
+  }, []);
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  if (!songs || !songs.length) {
+    return <div>No songs liked yet</div>;
+  }else{
+      return (
+        <div>
+          {songs.map(song => (
+            <Song key={song.Id} song={song} />
+          ))}
+        </div>
+      );
+  }
+
+}
+
+export default LikedSongs;
