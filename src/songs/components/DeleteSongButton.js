dbb3901df33221847858729b9f63932e15022327
Juan Carlos Cortés Muñoz <juacormun@alum.us.es>
2023-01-14 01:28:26 +0100

    Add delete song button only when user is admin

---
+++ src/songs/components/DeleteSongButton.js
@@ -0,0 +1,60 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { setLyrics, setInput } from "../slices/lyricsSlice";
+import { setAudioUrl, setVideoUrl } from "../slices/songMediaSlice";
+import { setSongs } from "../slices/songsSlice";
+
+function DeleteSongButton({ songId }) {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const songsState = useSelector((state) => state.songs);
+
+  async function deleteSong(songId) {
+    const request = new Request("/api/v1/songs/" + songId, {
+      method: "DELETE",
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("token")
+      },
+    });
+
+    const response = await fetch(request);
+
+    if (!response.ok) {
+      throw Error("Response not valid. " + response.status);
+    }
+
+    dispatch(setLyrics(""));
+    dispatch(setInput(""));
+    dispatch(setAudioUrl(""));
+    dispatch(setVideoUrl(""));
+    dispatch(setSongs(songsState.songs.filter((song) => song.id !== songId)));
+
+    navigate("/songs");
+  }
+
+  return (
+    <>
+      <a className="h1 darkBlueText align-top" href="#deleteSong" data-bs-toggle="modal" data-bs-target="#deleteSongModal">
+        <i className="bi bi-trash-fill"></i>
+      </a>
+      <div className="modal fade" id="deleteSongModal" tabIndex="-1" aria-labelledby="deleteSongModal" aria-hidden="true">
+        <div className="modal-dialog modal-dialog-centered modal-lg">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5">Delete song</h1>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+              <p>Are you sure you want to delete this song?</p>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn border-purple text-purple bg-blue" data-bs-dismiss="modal" onClick={() => deleteSong(songId)}>Delete</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default DeleteSongButton;
--- src/songs/components/SongDetail.js
+++ src/songs/components/SongDetail.js
@@ -1,13 +1,15 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-
 import { setLyrics, setInput } from "../slices/lyricsSlice";
 import { setAudioUrl, setVideoUrl } from "../slices/songMediaSlice";
+import { youtubeParser } from "./SongVideo";
+
 import LikeButton from "./LikeButton";
 import SongLyrics from "./SongLyrics";
 import SongAudio from "./SongAudio";
 import SongVideo from "./SongVideo";
+import DeleteSongButton from "./DeleteSongButton";
 
 function SongDetail() {
   const { id } = useParams();
@@ -44,31 +46,46 @@ function SongDetail() {
 
   return (
     <div className="row">
-      <div className="col-8 offset-2">
+      <div className="card border-0 col-8 offset-2card border-0 col-12 col-sm-10 offset-sm-1 col-md-8 offset-md-2">
         <div className="d-flex flex-column">
           <img src={song?.albumCover} className="card-img-top" alt="..." />
-          <div className="d-flex justify-content-between">
-            <p>
-              <LikeButton id={id} />
-            </p>
-          </div>
-          <h2 className="card-title">{song?.title}</h2>
-          <p className="card-text">{song?.artists.join(", ")}</p>
-          {!media?.videoUrl ? (
-            <div className="row">
-              <div className="col-12">
-                <SongAudio />
+          <div className="card-body">
+            <div className="d-flex justify-content-between">
+              <p>
+                <LikeButton id={id} />
+              </p>
+            </div>
+            {JSON.parse(localStorage.getItem("user")).role === 'admin' ? (
+              <div className="d-flex justify-content-between">
+                <div className="col-10 col-sm-8">
+                  <h2 className="card-title">{song?.title}</h2>
+                </div>
+                <div className="col-2 col-sm-4 d-flex justify-content-end">
+                  <DeleteSongButton songId={id} />
+                </div>
               </div>
-            </div>
-          ) : (
-            <></>
-          )}
-          <div className="row">
-            <div className="col-12 col-lg-6">
-              <SongVideo song={song} />
-            </div>
-            <div className="col-12 col-lg-6 order-lg-first">
-              <SongLyrics song={song} text={text} />
+            ) : (
+              <div className="col-12">
+                <h2 className="card-title">{song?.title}</h2>
+              </div>
+            )}
+            <p className="card-text">{song?.artists.join(", ")}</p>
+            {!media?.videoUrl || !youtubeParser(media?.videoUrl) ? (
+              <div className="row">
+                <div className="col-12">
+                  <SongAudio />
+                </div>
+              </div>
+            ) : (
+              <></>
+            )}
+            <div className="row">
+              <div className="col-12 col-lg-6">
+                <SongVideo song={song} />
+              </div>
+              <div className="col-12 col-lg-6 order-lg-first">
+                <SongLyrics song={song} text={text} />
+              </div>
             </div>
           </div>
         </div>
