5b8f4235b29e760a832f809330840a95fca01b19
carnuare <carnuare@gmail.com>
2023-01-15 13:48:21 +0100

    Add token listener & username listener

---
+++ src/common/components/UsernameListener.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react';
+
+export default function UsernameListener() {
+  const [username, setUsername] = useState("Log in");
+
+  useEffect(() => {
+    const checkUser = setInterval(() => {
+      let user = localStorage.getItem('user');
+      if (user) {
+        // wait 1 second to avoid flickering
+        user = JSON.parse(user);
+        setUsername(user.username);
+        clearInterval(checkUser);
+      }else {
+        setUsername("Log in");
+      }
+    }, 2000);
+
+    return () => clearInterval(checkUser);
+  }, []);
+
+return <a>{username}</a>;
+
+}
